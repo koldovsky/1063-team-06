905f952ea39654dd9f855a6552d86382dbc8e07b
JagnaJ <105728223+JagnaJ@users.noreply.github.com>
2023-09-17 00:13:48 +0200

    Create tasks-06.js

---
+++ js-tasks/tasks-06.js
@@ -0,0 +1,17 @@
+Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+/*code*/
+
+Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+/*code*/
+
+Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+/*code*/
+
+Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+/*code*/
+
+Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+/*code*/
+
+Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+/*code*/
