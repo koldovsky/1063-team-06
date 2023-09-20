4a49388cd8f71432e9fb3aae263bd36bf95819dd
Tetiana Pavlova <pavlovatania97@gmail.com>
2023-09-20 17:12:04 +0300

    added solutions to tasks from codewars

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,35 +1,3 @@
-// Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-}
-
-// Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
-function makeNegative(num) {
-  return num <= 0 ? num : -num;
-}
-
-// Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
-function move(position, roll) {
-  return position + roll * 2;
-}
-
-// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
-function greet(name, owner) {
-  return "Hello " + (name == owner ? "boss" : "guest");
-}
-
-// Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
-function litres(time) {
-  return Math.floor(time * 0.5);
-}
-
-// Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
-function lovefunc(flower1, flower2) {
-  return (flower1 + flower2) % 2 !== 0;
-}
-
-//========[ЗАВДАННЯ НА СЕРЕДУ]=============================================================
-
 // 1. Convert a string to an array https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 function stringToArray(string) {
   return string.split(" ");
