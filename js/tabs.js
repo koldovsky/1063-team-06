12e5c0872729bff2690b977b11c85b2730bce094
IlonaShh <143405641+IlonaShh@users.noreply.github.com>
2023-09-24 21:55:26 +0000

    added js tabs folder

---
+++ js/tabs.js
@@ -0,0 +1,10 @@
+/*const tabsBtn = document.querySelectorAll(".navigation-tab__tablinks");
+
+function onTabClick(tabcontent) {
+    tabcontent.addEventListener("click", function() {
+        let currentBtn = tabcontent;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+    });
+}
+*/
