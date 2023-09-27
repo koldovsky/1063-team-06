acd6877432b6a8909194e6a4202b9e393bd8e041
Iryna <irina_krupenina@ukr.net>
2023-09-27 16:51:01 +0300

    add additional settings for carousel

--- js/beautiful-landscapes-carousel.js
+++ js/beautiful-landscapes-carousel.js
@@ -7,55 +7,55 @@ const slides = [
     '<div><img src="img/landscapes/landscapes-carousel-6.jpg" alt="Red houses between mountains"></div>'
 ];
 
-let currentSlide = 0;
+let currentSlideIdx = 0;
 
 function renderSlide() {
-    const slideContainer = document.querySelector(".landscapes__carousel-slide");
-    slideContainer.innerHTML = slides[currentSlide];
-    console.log(slideContainer);
-    if (window.matchMedia("(min-width: 767px)").matches) {
-        const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-        slideContainer.innerHTML += slides[secondSlide];
-        if (window.matchMedia("(min-width:997px)").matches) {
-            const thirdSlide = secondSlide + 1 >= slideContainer.length ? 0 : secondSlide + 1;
-            slideContainer.innerHTML += slides[thirdSlide];
+    const slideContainer = document.querySelector('.landscapes__carousel-slide');
+    slideContainer.innerHTML = slides[currentSlideIdx];
+    // console.log(slideContainer);
+    if (window.matchMedia('(min-width: 767px)').matches) {
+        const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+        slideContainer.innerHTML += slides[secondSlideIdx];
+        if (window.matchMedia('(min-width: 997px)').matches) {
+            const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+            slideContainer.innerHTML += slides[thirdSlideIdx];
         }
     }
     renderIndicators();
 }
 
+window.addEventListener('resize', renderSlide);
+
 function nextSlide() {
-    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+    currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
     renderSlide();
 }
 
 function prevSlide() {
-    currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1;
+    currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
     renderSlide();
 }
 
 renderSlide();
-const nextBtn = document.querySelector(".landscapes__carousel-btn-next");
-nextBtn.addEventListener("click", nextSlide);
-const prevBtn = document.querySelector(".landscapes__carousel-btn-prev");
-prevBtn.addEventListener("click", prevSlide);
 
-window.addEventListener("resize", renderSlide);
+const nextBtn = document.querySelector('.landscapes__carousel-btn-next');
+nextBtn.addEventListener('click', nextSlide);
+const prevBtn = document.querySelector('.landscapes__carousel-btn-prev');
+prevBtn.addEventListener('click', prevSlide);
 
 function renderIndicators() {
-    const indicatorsContainer = document.querySelector(".landscapes__carousel-indicators");
-    indicatorsContainer.innerHTML = "";
+    const indicatorsContainer = document.querySelector('.landscapes__carousel-indicators');
+    indicatorsContainer.innerHTML = '';
     for (let i = 0; i < slides.length; i++) {
-      indicatorsContainer.innerHTML += 
-      `<button class="landscapes__carousel-indicator ${i === currentSlide ? "landscapes__carousel-indicator--active" : ""}"></button>`
+        indicatorsContainer.innerHTML += `<button class='landscapes__carousel-indicator ${i === currentSlideIdx ? 'landscapes__carousel-indicator--active' : ''}'></button>`
     }
-    const indicators = document.querySelectorAll(".landscapes__carousel-indicator");
+    const indicators = document.querySelectorAll('.landscapes__carousel-indicator');
     indicators.forEach((indicator, index) => {
-      indicator.addEventListener("click", () => {
-        currentSlide = index;
-        renderSlide();
-        renderIndicators(slides, currentSlide);
-      });
+        indicator.addEventListener('click', () => {
+            currentSlideIdx = index;
+            renderSlide();
+            renderIndicators(slides, currentSlide);
+        });
     });
 }
   
