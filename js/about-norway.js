4951a78a7aee2cc0a016e3a644f4b3149c62fb30
Viktoria <67107842+VikkiNau@users.noreply.github.com>
2023-10-02 10:40:35 +0000

    Adout Norway finished

--- js/about-norway.js
+++ js/about-norway.js
@@ -6,17 +6,43 @@ const aboutNorwayFact = [
     },
     {
         id: 2,
+        aboutNorwayFactNumber: `36`,
+        aboutNorwayFactText: `Kilometers of coastal strip`,
+    },
+    {
+        id: 3,
         aboutNorwayFactNumber: `450,000+`,
         aboutNorwayFactText: `Freshwater lakes`,
     },
     {
-        id: 3,
+        id: 4,
         aboutNorwayFactNumber: `10,000`,
         aboutNorwayFactText: `Waterfalls`,
     },
 ];
 
+
+const backgroundImages =[
+    '../img/about_norway/forest-grey.jpg',
+    '../img/about_norway/foggy-forest-grey.jpg',
+    '../img/about_norway/norway_forest.jpg',
+    '../img/about_norway/norway_mounts.jpg',
+    '../img/about_norway/tromso.jpg',
+]
+
+let currentImageIndex = 0;
+const backgrounContainer = document.querySelector('.about-norway__letter');
+    
+function changeBackgroun(){
+    backgrounContainer.style.backgroundImage = `URL(${backgroundImages[currentImageIndex]})`
+    currentImageIndex = (currentImageIndex+1)%backgroundImages.length;
+}
+
+
+setInterval(changeBackgroun, 3000);
+
 renderAboutNorweyFact(aboutNorwayFact);
+
 
 function renderAboutNorweyFact(aboutNorwayFact) {
     const norwayFactContainer = document.querySelector('.about-norway__fact-list');
